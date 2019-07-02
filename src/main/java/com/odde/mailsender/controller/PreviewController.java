1615270d2d52db4a5c3c8e9634c20b9ceab055dd
csduser <hoge@example.com>
2019-07-02 14:56:13 +0900

    add address required test

--- src/main/java/com/odde/mailsender/controller/PreviewController.java
+++ src/main/java/com/odde/mailsender/controller/PreviewController.java
@@ -1,6 +1,7 @@
 package com.odde.mailsender.controller;
 
 import com.odde.mailsender.form.MailSendForm;
+import com.odde.mailsender.form.PreviewForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -8,11 +9,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+
 @Controller
 public class PreviewController {
 
     @PostMapping("/preview")
-    public String preview(@ModelAttribute("form") MailSendForm form, BindingResult result, Model model) {
+    public String preview(@Valid @ModelAttribute("form") PreviewForm form, BindingResult result, Model model) {
+
+        if (result.hasErrors()) {
+            // TODO 後で見直す。
+            return "preview";
+        }
+
         return "preview";
     }
 }
---
+++ src/main/java/com/odde/mailsender/service/PreviewInfo.java
@@ -0,0 +1,41 @@
+package com.odde.mailsender.service;
+
+public class PreviewInfo {
+    private String to;
+    private String subject;
+    private String body;
+
+    public PreviewInfo() {
+    }
+
+    public PreviewInfo(String to, String subject, String body) {
+        this.to = to;
+        this.subject = subject;
+        this.body = body;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+}
