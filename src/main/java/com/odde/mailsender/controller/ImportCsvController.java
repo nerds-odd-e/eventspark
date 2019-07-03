e34d5e0073e975407f722724edba6d90b77aa790
d.kanai <d.kanai@pastoraldog.com>
2019-07-03 10:30:54 +0900

    Add ImportCsvController

--- src/main/java/com/odde/mailsender/controller/ImportCsvController.java
+++ src/main/java/com/odde/mailsender/controller/ImportCsvController.java
@@ -8,12 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class ImportCsvController {
@@ -27,4 +28,12 @@ public class ImportCsvController {
         return "import-csv";
     }
 
+    @PostMapping("/import-csv")
+    public ModelAndView postCsv(
+            @RequestParam("file") MultipartFile multipartFile,
+            @RequestParam("force") String force) {
+        ModelAndView model = new ModelAndView("contact-list");
+        model.addObject("form", new ContactListForm());
+        return model;
+    }
 }
