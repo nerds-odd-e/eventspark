4d087a381a63f85dd44e2dbf276e3e75d45444cd
Keita Suzuki <leonsk32@gmail.com>
2019-07-03 13:38:20 +0900

    implement getTemplate method

--- src/main/java/com/odde/mailsender/service/MailTemplate.java
+++ src/main/java/com/odde/mailsender/service/MailTemplate.java
@@ -3,6 +3,8 @@ package com.odde.mailsender.service;
 import java.util.Objects;
 
 public class MailTemplate {
+    public static final String TEMPLATE_SUBJECT = "hello $name";
+    public static final String TEMPLATE_BODY = "hello $name, this is the fixed template.";
     private String subject;
     private String body;
 
