93ea2bab40f0dbbdc9dfd544847cb0903c8af145
csduser <hoge@example.com>
2019-07-04 17:16:46 +0900

    refactor mail controller

--- src/main/java/com/odde/mailsender/form/MailSendForm.java
+++ src/main/java/com/odde/mailsender/form/MailSendForm.java
@@ -1,14 +1,16 @@
 package com.odde.mailsender.form;
 
 import com.odde.mailsender.data.AddressItem;
+import com.odde.mailsender.service.AddressBookService;
 import com.odde.mailsender.service.MailInfo;
 import org.apache.commons.lang3.StringUtils;
-import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MailSendForm {
 
@@ -78,7 +80,33 @@ public class MailSendForm {
         return new MailInfo("eventspark@gmx.com", addressItem.getMailAddress(), renderSubjectTemplate(addressItem), renderBodyTemplate(addressItem));
     }
 
-    public MailInfo createNormalMail(String address) {
+    private MailInfo createNormalMail(String address) {
         return new MailInfo("eventspark@gmx.com", address, getSubject(), getBody());
+    }
+
+    public List<MailInfo> getMailInfoList(AddressBookService addressBookService) throws Exception {
+        List<MailInfo> mailInfoList;
+        if (isTemplate()) {
+            mailInfoList = getTemplateMailInfoList(addressBookService);
+        } else {
+            mailInfoList = getNormalMailInfoList();
+        }
+        return mailInfoList;
+    }
+
+    private List<MailInfo> getTemplateMailInfoList(AddressBookService addressBookService) throws Exception {
+        List<MailInfo> mailInfoList = new ArrayList<>();
+        for (String address : getAddresses()) {
+            AddressItem addressItem = addressBookService.findByAddress(address);
+            if (addressItem == null || StringUtils.isEmpty(addressItem.getName()))
+                throw new Exception("When you use template, choose email from contract list that has a name");
+
+            mailInfoList.add(createRenderedMail(addressBookService.findByAddress(address)));
+        }
+        return mailInfoList;
+    }
+
+    private List<MailInfo> getNormalMailInfoList() {
+        return Arrays.stream(getAddresses()).map(this::createNormalMail).collect(Collectors.toList());
     }
 }
