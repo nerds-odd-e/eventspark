8f75313b6b2a320b11324f33e3fc58647a4a65d3
t.ozaki <ozaki_yjm@yahoo.co.jp>
2019-07-05 15:02:27 +0900

    fixed import.

--- src/test/java/com/odde/mailsender/controller/ImportCsvControllerTest.java
+++ src/test/java/com/odde/mailsender/controller/ImportCsvControllerTest.java
@@ -23,8 +23,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
