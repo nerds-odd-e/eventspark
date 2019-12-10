ee3d8d1bbc47b106a233dfc623e3036d25ee2b25
teamss1 <peace@csd.fun>
2019-12-10 15:40:16 +0900

    Refactor event detail page

--- src/main/java/com/odde/mailsender/controller/EventController.java
+++ src/main/java/com/odde/mailsender/controller/EventController.java
@@ -1,14 +1,24 @@
 package com.odde.mailsender.controller;
 
+import com.odde.mailsender.data.Event;
+import com.odde.mailsender.service.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class EventController {
-
+    @Autowired
+    private EventRepository eventRepository;
     @GetMapping("/event/{title}")
-    public String showDetail(@PathVariable("title") String title) {
+    public String showDetail(@PathVariable("title") String title, Model model) {
+
+        Event event = eventRepository.findByEventName(title);
+        model.addAttribute("event", event);
+
         return "event-detail";
     }
 
