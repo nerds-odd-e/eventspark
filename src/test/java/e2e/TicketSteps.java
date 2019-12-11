421cf980cd7df62c6e840b0990bb2f3706f64606
csduser <a@a.com>
2019-12-11 15:53:36 +0900

    modify Ticket feature

--- src/test/java/e2e/TicketSteps.java
+++ src/test/java/e2e/TicketSteps.java
@@ -24,45 +24,36 @@ public class TicketSteps {
         @Autowired
         private AddTicketPage addTicketPage;
 
-        @When("以下の入力を行う")
-        public void 以下の入力を行う(io.cucumber.datatable.DataTable dataTable) {
+        @When("以下の入力を行い「登録」ボタンをクリックすると")
+        public void 以下の入力を行い_登録_ボタンをクリックすると(io.cucumber.datatable.DataTable dataTable) {
             Map<Object, String> table = dataTable.asMap(String.class, String.class);
             addTicketPage.fillTicketName(table.get("チケット名"));
             addTicketPage.fillTicketPrice(table.get("金額"));
             addTicketPage.fillTicketTotal(table.get("枚数"));
             addTicketPage.fillTicketLimit(table.get("上限数"));
-        }
-
-        @When("「登録」ボタンをクリックする")
-        public void 登録_ボタンをクリックする() {
             addTicketPage.submit();
         }
 
-        @Then("イベント詳細ページに戻る")
-        public void イベント詳細ページに戻る() {
+        @Then("イベント詳細ページが表示され")
+        public void イベント詳細ページが表示され() {
             // Write code here that turns the phrase above into concrete actions
             throw new cucumber.api.PendingException();
         }
 
-        @Then("入力したチケット情報が表示されている")
-        public void 入力したチケット情報が表示されている() {
+        @Then("イベント詳細ページに入力したチケット情報が表示されている")
+        public void イベント詳細ページに入力したチケット情報が表示されている() {
             // Write code here that turns the phrase above into concrete actions
             throw new cucumber.api.PendingException();
         }
 
-        @Then("画面遷移しない")
-        public void 画面遷移しない() {
-            // Write code here that turns the phrase above into concrete actions
-            throw new cucumber.api.PendingException();
-        }
     }
 
     public static class 全ての項目を空で登録ボタンを押下 {
         @Autowired
         private AddTicketPage addTicketPage;
 
-        @When("全てのフォームを空にして、「登録」ボタンをクリックする")
-        public void 全てのフォームを空にして_登録_ボタンをクリックする() {
+        @When("全てのフォームを空にして「登録」ボタンをクリックすると")
+        public void 全てのフォームを空にして_登録_ボタンをクリックすると() {
             addTicketPage.fillTicketName("");
             addTicketPage.fillTicketPrice("");
             addTicketPage.fillTicketTotal("");
@@ -70,26 +61,32 @@ public class TicketSteps {
             addTicketPage.submit();
         }
 
-        @Then("画面上に「チケット名を入力してください」を表示する")
-        public void 画面上に_チケット名を入力してください_を表示する() {
+        @Then("チケット登録画面が表示された状態で")
+        public void チケット登録画面が表示された状態で() {
             // Write code here that turns the phrase above into concrete actions
             throw new cucumber.api.PendingException();
         }
 
-        @Then("画面上に「金額を入力してください」を表示する")
-        public void 画面上に_金額を入力してください_を表示する() {
+        @Then("画面上に「チケット名を入力してください」が表示され")
+        public void 画面上に_チケット名を入力してください_が表示され() {
             // Write code here that turns the phrase above into concrete actions
             throw new cucumber.api.PendingException();
         }
 
-        @Then("画面上に「枚数を入力してください」を表示する")
-        public void 画面上に_枚数を入力してください_を表示する() {
+        @Then("画面上に「金額を入力してください」が表示され")
+        public void 画面上に_金額を入力してください_が表示され() {
             // Write code here that turns the phrase above into concrete actions
             throw new cucumber.api.PendingException();
         }
 
-        @Then("画面上に「一人当たりの上限数を入力してください」を表示する")
-        public void 画面上に_一人当たりの上限数を入力してください_を表示する() {
+        @Then("画面上に「枚数を入力してください」が表示され")
+        public void 画面上に_枚数を入力してください_が表示され() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new cucumber.api.PendingException();
+        }
+
+        @Then("画面上に「一人当たりの上限数を入力してください」が表示される")
+        public void 画面上に_一人当たりの上限数を入力してください_が表示される() {
             // Write code here that turns the phrase above into concrete actions
             throw new cucumber.api.PendingException();
         }
