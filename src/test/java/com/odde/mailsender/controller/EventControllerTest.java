726ad5c623713d6d4c4233edffc842336728e7ca
teamss3 <hoge@example.com>
2019-12-11 15:53:06 +0900

    イベント追加ページ用コントローラのテストを追加

--- src/test/java/com/odde/mailsender/controller/EventControllerTest.java
+++ src/test/java/com/odde/mailsender/controller/EventControllerTest.java
@@ -11,7 +11,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
 import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
@@ -51,4 +55,11 @@ public class EventControllerTest {
         mvc.perform(get("/event/" + event.getName()))
                 .andExpect(model().attribute("event", event));
     }
+
+    @Test
+    public void displayAddEventPage() throws Exception {
+        MvcResult result = mvc.perform(get("/owner/event/new")).andReturn();
+        assertEquals(200,result.getResponse().getStatus());
+    }
+
 }
