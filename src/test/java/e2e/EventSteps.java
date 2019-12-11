3a2d03f212fd826124bb01e24f8ca5cd98349dd0
teamss3 <hoge@example.com>
2019-12-11 11:05:46 +0900

    Add Eventをリファクタリングしたシナリオテストに反映

--- src/test/java/e2e/EventSteps.java
+++ src/test/java/e2e/EventSteps.java
@@ -84,26 +84,26 @@ public class EventSteps {
         throw new cucumber.api.PendingException();
     }
 
+    @Given("イベント追加ページを表示する")
+    public void イベント追加ページを表示する() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
     @When("イベント追加ページにゴスペルワークショップの情報を入力する")
     public void イベント追加ページにゴスペルワークショップの情報を入力する() {
         // Write code here that turns the phrase above into concrete actions
         throw new cucumber.api.PendingException();
     }
 
-    @Then("入力したゴスペルワークショップの情報がDBに登録される")
-    public void 入力したゴスペルワークショップの情報がdbに登録される() {
+    @Then("新しいイベントが追加される")
+    public void 新しいイベントが追加される() {
         // Write code here that turns the phrase above into concrete actions
         throw new cucumber.api.PendingException();
     }
 
     @Then("入力したゴスペルワークショップの情報が詳細ページに表示される")
     public void 入力したゴスペルワークショップの情報が詳細ページに表示される() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
-    }
-
-    @When("イベント追加ページを表示する")
-    public void イベント追加ページを表示する() {
         // Write code here that turns the phrase above into concrete actions
         throw new cucumber.api.PendingException();
     }
