34290fda714c35bffb5faeaa74f8e572b72b6310
csduser <a@a>
2019-12-12 16:16:00 +0900

    eventRegisterコントローラーにチケット上限のバリデーションチェックを

--- src/main/java/com/odde/mailsender/controller/RegisterController.java
+++ src/main/java/com/odde/mailsender/controller/RegisterController.java
@@ -10,6 +10,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.xml.transform.OutputKeys;
+
 @Controller
 public class RegisterController {
 
@@ -18,10 +20,21 @@ public class RegisterController {
 
     @PostMapping("/register")
     public String registerToEvent(@ModelAttribute("form") RegisterForm form, BindingResult result, Model model) {
+
+        //チケット全体の購入数の上限チェックしたい
+
+        //１人あたりのチケット購入上限チェックしたい
+
+        //OKだったら参加登録可能
         registrationInfoRepository.save(new RegistrationInfo(form.getFirstName(), form.getLastName(), form.getCompany(), form.getAddress(), form.getTicketType(),
                 form.getTicketCount(), form.getEventId()));
 
         return "redirect:/register_complete";
+    }
+
+    public boolean isBuyableForTicketMaximum(int ticketMaximum, Integer ticketSoled, Integer ticketBought)
+    {
+        return ticketMaximum >= (ticketSoled + ticketBought);
     }
 
 }
--- src/test/java/com/odde/mailsender/controller/RegisterControllerTest.java
+++ src/test/java/com/odde/mailsender/controller/RegisterControllerTest.java
@@ -1,5 +1,6 @@
 package com.odde.mailsender.controller;
 
+import com.odde.mailsender.data.Event;
 import com.odde.mailsender.data.RegistrationInfo;
 import com.odde.mailsender.service.RegistrationInfoRepository;
 import org.junit.Before;
@@ -11,11 +12,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
@@ -31,6 +32,9 @@ public class RegisterControllerTest {
     @Autowired
     private RegistrationInfoRepository registrationInfoRepository;
 
+    @Autowired
+    private RegisterController registerController;
+
     @Before
     public void setup() {
         registrationInfoRepository.deleteAll();
@@ -45,8 +49,7 @@ public class RegisterControllerTest {
                 .param("address", "aaa@example.com")
                 .param("ticketType", "1day")
                 .param("ticketCount", "1")
-                .param("eventId", "event"))
-                .andExpect(redirectedUrl("/register_complete"));
+                .param("eventId", "event"));
 
         List<RegistrationInfo> all = registrationInfoRepository.findAll();
 
@@ -59,6 +62,16 @@ public class RegisterControllerTest {
         assertEquals(Integer.valueOf(1), all.get(0).getTicketCount());
         assertEquals("event", all.get(0).getEventId());
         assertNotNull(all.get(0).getId());
+    }
+
+    @Test
+    public void チケット数が100分の99枚の場合1枚購入出来る() {
+        assertTrue(registerController.isBuyableForTicketMaximum(100, 99, 1));
+    }
+
+    @Test
+    public void チケット数が100分の100枚の場合1枚購入出来ない() {
+        assertFalse(registerController.isBuyableForTicketMaximum(100, 100, 1));
     }
 
 }
