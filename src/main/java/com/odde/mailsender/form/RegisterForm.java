97fe6c20531c1d3e9eb01cf4994a067bb314876a
csduser <a@a>
2019-12-12 10:41:15 +0900

    RegistrationFormの姓名、会社名対応 * プロパティを追加、変更

--- src/main/java/com/odde/mailsender/form/RegisterForm.java
+++ src/main/java/com/odde/mailsender/form/RegisterForm.java
@@ -2,7 +2,11 @@ package com.odde.mailsender.form;
 
 public class RegisterForm {
 
-    private String name;
+    private String firstName;
+
+    private String lastName;
+
+    private String company;
 
     private String address;
 
@@ -12,12 +16,29 @@ public class RegisterForm {
 
     private String eventId;
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
     }
 
     public String getAddress() {
